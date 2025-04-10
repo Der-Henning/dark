8332655883109674f92b0712afdd6b8de1d6d778
Henning Merklinger <henning.merklinger@gmail.com>
2025-04-10 16:58:57 +0200

    add labyrinth doc

--- dark/labyrinth.py
+++ dark/labyrinth.py
@@ -5,6 +5,11 @@ import random
 
 class Cell:
     def __init__(self, position: tuple[int, int]):
+        """Initialize new cell.
+        
+        Create all surrounding edges.
+        Create an empty set of all connected cells and add self.
+        """
         self.position = position
         self.edges = set([
             Edge(position, (position[0] + 1, position[1])),
@@ -50,6 +55,11 @@ class Grid:
         self.reset()
 
     def reset(self):
+        """Initialize Grid.
+        
+        Create width x height cells and create all edges.
+        For each edge store the adjacent cells in a dictionary.
+        """
         self.edges: dict[Edge, list[Cell]] = dict()
         for x in range(self.width):  
             for y in range(self.height):
@@ -66,14 +76,21 @@ class Labyrinth:
         self.grid = Grid(width, height)
 
     def generate(self) -> list[Edge]:
+        """Generate Labyrinth by removing random edges of a grid until all cells are connected.
+        """
+        # Initialize Grid
         self.grid.reset()
         edges = list(self.grid.edges.keys())
         new_edges: list[Edge] = []
+
+        # Schuffle edges
         random.shuffle(edges)
 
+        # iterate over all edges and remove them form the original set until its empty.
         while edges:
             edge = edges.pop()
             
+            # When edge is a border edge, add it to final set
             if len(self.grid.edges[edge]) < 2:
                 new_edges.append(edge)
                 continue
@@ -81,11 +98,14 @@ class Labyrinth:
             cell1 = self.grid.edges[edge][0]
             cell2 = self.grid.edges[edge][1]
 
+            # When adjacent cells of the edge are not connected remove edge (dont add to final set)
+            # and update the sets of all newly connected cells.
             if cell1.set != cell2.set:
                 for cell in cell1.set.copy():
                     cell.set.update(cell2.set)  
                 for cell in cell2.set.copy():
                     cell.set.update(cell1.set)
+            # When cells are connected, add edge to final set
             else:
                 new_edges.append(edge)
             
